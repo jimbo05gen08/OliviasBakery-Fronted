caf6fc9646841b2cc1636958d8aab9e91207429b
jimbo05gen08 <jimmy.obregon@3ek.pe>
2025-02-01 10:24:27 -0500

    subiendo cambios del dia 01/02/2025

---
+++ src/app/models/pasarela/pasarela.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-pasarela',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './pasarela.component.html',
+  styleUrl: './pasarela.component.scss',
+})
+export class PasarelaComponent {}
--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,18 +1,102 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CartItemComponent } from './ui/cart-item/cart-item.component';
 import { CartStateService } from '../../shared/services/cart-state.service';
 import { ProductItemCart } from '../../shared/interfaces/product.interface';
 import { CurrencyPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { PasarelaComponent } from '../../models/pasarela/pasarela.component';
+import { FormsModule } from '@angular/forms';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  state,
+} from '@angular/animations';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartItemComponent, CurrencyPipe, RouterLink],
+  imports: [
+    CartItemComponent,
+    CurrencyPipe,
+    RouterLink,
+    PasarelaComponent,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './cart.component.html',
-  styles: ``,
+  styleUrl: './cart.component.scss',
+  animations: [
+    trigger('estadoInicial', [
+      state(
+        'void',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+        }),
+      ),
+      transition(':enter', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(0)',
+            opacity: 1,
+          }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export default class CartComponent {
+  generatePDF() {
+    const data = document.getElementById('contenido-factura-pdf');
+    if (data) {
+      html2canvas(data)
+        .then((canvas) => {
+          const imgWidth = 210;
+          const imgHeight = (canvas.height * imgWidth) / canvas.width;
+          const imgData = canvas.toDataURL('image/png');
+          const doc = new jsPDF();
+
+          doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+          doc.save('boleta-de-pago.pdf');
+        })
+        .catch((error) => {
+          console.error('Error generating PDF:', error);
+        });
+    } else {
+      console.error('Elemento con el ID "contenido-factura" no encontrado.');
+    }
+  }
+
+  paymentForm = {
+    paymentType: '',
+    accountType: '',
+    cardNumber: '',
+    ccv: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    expiryMonth: '',
+    expiryYear: '',
+  };
+
+  tab1Visible = true; // Visible por defecto
+  tab2Visible = false;
+  tab3Visible = false;
+  tab4Visible = false;
+
+  showTab(tabNumber: number) {
+    this.tab1Visible = tabNumber === 1;
+    this.tab2Visible = tabNumber === 2;
+    this.tab3Visible = tabNumber === 3;
+    this.tab4Visible = tabNumber === 4;
+  }
+
   state = inject(CartStateService).state;
 
   onRemove(id: number) {
@@ -31,5 +115,14 @@ export default class CartComponent {
       ...product,
       quantity: product.quantity - 1,
     });
+  }
+
+  canProceed(): boolean {
+    return this.state.price() > 0;
+  }
+
+  onSubmit() {
+    // Aquí puedes agregar la lógica para procesar el pago
+    console.log('Formulario enviado', this.paymentForm);
   }
 }
